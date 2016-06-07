5e11a177b396a0cc2a0aab88a74d6a1257ddb2c0
jamesn <jamesn@26291-E7450>
2016-06-07 09:07:21 -0700

    Updates

--- TMobileChatApp/Controllers/ChatsController.cs
+++ TMobileChatApp/Controllers/ChatsController.cs
@@ -20,41 +20,6 @@ namespace TMobileChatApp.Controllers
     {
         private TMobileChatAppContext db = new TMobileChatAppContext();
 
-        //// PUT: api/Chats/5
-        //[ResponseType(typeof(void))]
-        //public async Task<IHttpActionResult> PutChat(int id, Chat chat)
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return BadRequest(ModelState);
-        //    }
-
-        //    if (id != chat.ChatId)
-        //    {
-        //        return BadRequest();
-        //    }
-
-        //    db.Entry(chat).State = EntityState.Modified;
-
-        //    try
-        //    {
-        //        await db.SaveChangesAsync();
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //        if (!ChatExists(id))
-        //        {
-        //            return NotFound();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
-
-        //    return StatusCode(HttpStatusCode.NoContent);
-        //}
-
         // POST: api/Chats
         [ResponseType(typeof(void))]
         public async Task<IHttpActionResult> PostChat(Chat chat)
@@ -111,7 +76,7 @@ namespace TMobileChatApp.Controllers
             CookieHeaderValue rtFaCookie = Request.Headers.GetCookies("rtFa").FirstOrDefault();
             if (authCookie != null && rtFaCookie != null)
             {
-                //string authCookieStr = "77u/PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48U1A+RmFsc2UsMGguZnxtZW1iZXJzaGlwfDEwMDMwMDAwOGJmOTNkNzRAbGl2ZS5jb20sMCMuZnxtZW1iZXJzaGlwfHNhaXNAc2xhbG9tY29uc3VsdGluZy5vbm1pY3Jvc29mdC5jb20sMTMxMTAxNjExNDY4MjI3MTczOzEzMTA2OTQ2MjczMDAwMDAwMCxGYWxzZSx2ZnF0Z3QrOHgxdDhyU0x1N3RHTzFPL3c0ZDFBM2lIQ2gzeDE1dytsVDJWbzZvRnY3dENOMEkwWXpqek5vZjQzYW9lWmN0ejRzYzNNeEVobmYxVDBjcXI4czFiVnA5cFFUbFpaUE4vTlMvbW4xbEVWUlBmQ1RvMDBBekpMVndpaWdNaXNzZVIzWkQzakpFOHAwS3BHV1dNbDI2WDd2MTdjajlaaXdkazdSaUQ3b3Q0N2JYRDVhb2o3U2lrM0g3TGN2Qzgyc000TlNsOGVjWEQvZ0F5NWFyNmwwUCs5eTdsekViQ3dDT1E4aXFLb0hhMnRmNU8xUFlWNmd5ck1QYkVDZjZWRTd4NkRlbzY0ZUxSSmNKN09qZ2VzWE1PQW9DMm5oelVCZXpsdm9MTkd5aWluSzV0K1hlWWFqY1dHb2I0ZklJNzRnWDY1ZzlpTTFINXAvR3I1T1E9PSxodHRwczovL3NsYWxvbWNvbnN1bHRpbmcuc2hhcmVwb2ludC5jb20vc2l0ZXMvdG1vaW50cmFuZXQ8L1NQPg==";
+                //string authCookieStr = "77u/PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48U1A+RmFsc2UsMGguZnxtZW1iZXJzaGlwfDEwMDMwMDAwOGJmOTNkNzRAbGl2ZS5jb20sMCMuZnxtZW1iZXJzaGlwfHNhaXNAc2xhbG9tY29uc3VsdGluZy5vbm1pY3Jvc29mdC5jb20sMTMxMTAyMTg2NTcyMzQzMTczOzEzMTA2OTQ2MjczMDAwMDAwMCxGYWxzZSxXZnlhSHNPYVpVOUdpSElVR3JySGpTbTk0Y3hPOWdnbnZGYzhWdHhZbExJaVhxZGJQRmo0WitvU1l3TC9vUmwxSDlpTkNhelVTdFBRVmV0NmJtK3FpVVJrUlVDQWYrdVl6dVVDa0w2VTUzeDZMS0tscTYvc2h0NW13NWkyaC9XbGlzZlpBdDJsWFEwSG84bHFNV2dQN21FTG9aTE01Q1VDemNnelg5ZmNjcGNmODljV1VCUDBuYytnSk5NalBXQjJIRC95Tk1yMGZMZEVERXJ2b3hzS1R2V3p1SzZ4YnFZTldyM084THZMdHliRDFNamlvTjRHYjFKVHoyZWM2Yy83eTB2S1N3aFVEeTFvdVZta1N3NU1jNkFaYkNxMVk2cGdkTm9ZUEdFNzZJUHg4RktqYzNBY20raVhqRU5KelBuZ2FzWWJGYXl6QzNadjdXQlRFbFpLd2c9PSxodHRwczovL3NsYWxvbWNvbnN1bHRpbmcuc2hhcmVwb2ludC5jb20vc2l0ZXMvdG1vaW50cmFuZXQ8L1NQPg==";
                 //string rtFaCookieStr = "KKw6mBRxRfW/p2SkTdteFtC1VW4K4t/zvxzqrLMMdQMPNQKayqjBN4uG+bynInS/lOLDBD7zgNjJkvWqomcb3R6iI9W5pTYuxOZzbJTff5f2M9QoHgTUPzkdu9990HdhPJobXwncGA7I4d04Jw5yDUG+FdRKbKPlf39z6YQoI+0gIW/wPctgJSpsb0NJwhEsKGV7OEFlCy5JCZhw1ebcfguImdXhNz4SZOed7JOIlIBz6U9mc0clV3XqO3CgRmA04tEuRiwuOCbec2cWDbTgCACoJj0E50mc4Vej/mTKZelfGUqlFgyIdLncOQbAjQtd2Sl7Zr5HfZWwrP4eWQA/+QT142Lpc9v26aAPfZYVdS6qE3ON5CB14G/N8h/bkCAYIAAAAA==";
                 string authCookieStr = authCookie["FedAuth"].Value;
                 string rtFaCookieStr = rtFaCookie["rtFa"].Value;
@@ -132,10 +97,8 @@ namespace TMobileChatApp.Controllers
                     HttpResponseMessage response = await client.GetAsync("sites/tmointranet/_api/SP.UserProfiles.PeopleManager/GetMyProperties/UserProfileProperties");
                     if (response.IsSuccessStatusCode)
                     {
-                        //Product product = await response.Content.ReadAsAsync > Product > ();
-                        //Console.WriteLine("{0}\t${1}\t{2}", product.Name, product.Price, product.Category);
                         object personProperties = await response.Content.ReadAsAsync<object>();
-                        userId = ((personProperties as Newtonsoft.Json.Linq.JObject)["value"] as Newtonsoft.Json.Linq.JArray).Children().FirstOrDefault(c => c.Value<string>("Key") == "UserName").Value<string>("Value").ToString();
+                        userId = ((personProperties as Newtonsoft.Json.Linq.JObject)["value"] as Newtonsoft.Json.Linq.JArray).Children().FirstOrDefault(c => c.Value<string>("Key") == "EmployeeID").Value<string>("Value").ToString();
                     }
                     else
                     {
